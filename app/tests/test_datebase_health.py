cd93d4a2a8a59e7c3bf58229a8a7c4a670437e92
mirmakhmudov <mr.mirmakhmudov16112008@gmail.com>
2025-11-05 00:16:07 +0200

    Commit/ ci-cd.yml and fix some problem with pytest

---
+++ app/tests/test_datebase_health.py
@@ -0,0 +1,11 @@
+import pytest
+from httpx import AsyncClient, ASGITransport
+from app.main import app
+
+
+@pytest.mark.asyncio
+async def test_health_check():
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        res = await ac.get("/api/v1/system/health")
+    assert res.status_code == 200
